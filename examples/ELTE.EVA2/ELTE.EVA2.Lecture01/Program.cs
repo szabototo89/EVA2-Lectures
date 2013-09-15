72b0317672725813b2de3018509700fd16aa5c09
szabototo89 <szabototo89@gmail.com>
2013-09-15 22:22:59 +0200

    More info in lecture01

--- examples/ELTE.EVA2/ELTE.EVA2.Lecture01/Program.cs
+++ examples/ELTE.EVA2/ELTE.EVA2.Lecture01/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,10 +9,41 @@ namespace ELTE.EVA2.Lecture01
 {
     class Program
     {
+	    static void LogMessage(string message)
+	    {
+		    Console.WriteLine("Message logged: {0}", message);
+	    }
+
+		static void LogMessage(string msg1, string msg2)
+		{
+			LogMessage(msg1);
+			LogMessage(msg2);
+		}
+
+	    static void LogMessage(params string[] messages)
+	    {
+		    foreach (var message in messages)
+			    Console.WriteLine(message);
+	    }
+
         static void Main(string[] args)
         {
             var matrix = Matrix.GetIdenticalMatrix(10);
             Console.WriteLine(matrix.Add(Matrix.GetIdenticalMatrix(10)));
+
+	        LogMessage("Hello", "World");
+			LogMessage("Hello");
+			LogMessage(new[] { "Hello" });
+			LogMessage(new[] { "Hello", "World" });
+
+			string[] words = new string[] { "Hello", " ", "World", "!" };
+	        IEnumerator enumerator = words.GetEnumerator();
+			enumerator.Reset();
+	        while (enumerator.MoveNext())
+	        {
+		        string current = (string)enumerator.Current;
+		        Console.Write(current.ToUpper());
+	        }
         }
     }
 }
